46050a1af37a573abc76a4d1f8d4583e25188e66
Rafael Zomer <rafaelzomer@gmail.com>
2017-08-29 14:30:42 -0300

    testing

--- content/post/hello-world.md
+++ content/post/hello-world.md
@@ -1,7 +1,6 @@
 ---
 title: "Hello World"
 date: 2017-08-29T11:24:42-03:00
-draft: true
 ---
 
 Hello World
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# DIR=$(dirname "$0")
-
-# cd $DIR/..
-
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
@@ -26,4 +22,4 @@ echo "Generating site"
 hugo -t hugo-theme-casper
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push origin gh-pages
